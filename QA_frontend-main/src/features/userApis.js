a987a83e871410a2bc50176ad69c76eb2d141ca0
bicom1 <qc_dev@bicommunications.net>
2025-05-09 20:02:37 +0500

    package update

--- QA_frontend-main/src/features/userApis.js
+++ QA_frontend-main/src/features/userApis.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
 const cookie = new Cookies();
-const baseUrl = "https://backendbic-rrrm.onrender.com";
-// const baseUrl = "http://localhost:8000";
+// const baseUrl = "https://backendbic-rrrm.onrender.com";
+const baseUrl = "http://localhost:8000";
 export const LoginApi = async (data) => {
   const res = await axios.post(`${baseUrl}/login`, data);
   return res;
