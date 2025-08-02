a4eba8c0bd391084c1c1cfa5923e13f0e82a8c3b
haris <qc_dev@bicommunications.net>
2025-08-02 15:01:48 +0500

    update

--- QA_frontend-main/src/features/userApis.js
+++ QA_frontend-main/src/features/userApis.js
@@ -6,8 +6,8 @@ const cookie = new Cookies();
 // const baseUrl = "https://backendbic-rrrm.onrender.com";
 // const baseUrl = "https://backendbic-rrrm.onrender.com";
 
-const baseUrl = "https://backendbic-rrrm.onrender.com";
-// const baseUrl = "http://localhost:8000";
+// const baseUrl = "https://backendbic-rrrm.onrender.com";
+const baseUrl = "http://localhost:8000";
 
 export const LoginApi = async (data) => {
   const res = await axios.post(`${baseUrl}/login`, data);
